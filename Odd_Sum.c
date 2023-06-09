c982e7e8fc80e6e88d8ac01d99f114f66fd7b90d
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2023-06-09 20:30:23 +0530

    #include<stdio.h> int main() {     int n,a[100],i,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=0;i<n;i++){
        if(a[i]%2!=0){
            
            sum=sum+a[i];
        }
    }
        
        printf("%d",sum);
        
    }

--- Odd_Sum.c
+++ Odd_Sum.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-int main(){
-    int i,n,a[n];
-    int s=0;
+int main()
+{
+    int n,a[100],i,sum=0;
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++){
-        if(a[i]%2!=0){
-        s=s+a[i];
-        }
+	
+    if(a[i]%2!=0){
+        
+        sum=sum+a[i];
     }
-    printf("%d",s);
+}
+    
+    printf("%d",sum);
     
 }
