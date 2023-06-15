2f8c868ab4c3335bf1550e4f80ac22284bacd2bf
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2023-06-15 22:05:34 +0530

    #include<stdio.h> int main(){     int i,n,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++){         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++){         if(arr[i]%2!=0){             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++){         if(arr[i]%2==0){             printf("%d ",arr[i]);         }     } }

--- Array_of_odd_and_even_elements.c
+++ Array_of_odd_and_even_elements.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main(){
-	int n,a[100],i;
-	scanf("%d",&n);
-	for(i=0;i<n;i++){
-		scanf("%d",&a[i]);
-	}
-	for(i=0;i<n;i++){
-		if(a[i]%2!=0){
-			printf("%d ",a[i]);
-		}
-	}
-	for(i=0;i<n;i++){
-		if(a[i]%2==0){
-			printf("%d ",a[i]);
-		}
-	}
+    int i,n,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        if(arr[i]%2!=0){
+            printf("%d ",arr[i]);
+        }
+    }
+    for(i=0;i<n;i++){
+        if(arr[i]%2==0){
+            printf("%d ",arr[i]);
+        }
+    }
 }
