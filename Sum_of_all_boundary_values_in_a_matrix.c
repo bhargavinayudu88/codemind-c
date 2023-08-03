def1c6ec90be8e067b2a2be14441dbe5df01e4c3
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2023-08-03 14:05:44 +0530

    #include<stdio.h> int main() { 	int i,j,r,c,sum=0; 	scanf("%d%d",&r,&c); 	int mat[r][c]; 	for(i=0;i<r;i++){ 		for(j=0;j<c;j++){ 			scanf("%d",&mat[i][j]); 		} 	} 	for(i=0;i<r;i++){ 		for(j=0;j<c;j++){
    			if(i==0 || j==0 || i==r-1 || j==c-1){
    				sum=sum+mat[i][j];
    			}
    			
    		}
    	
    }
    	printf("%d",sum);
    }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,r,c,sum=0;
+	scanf("%d%d",&r,&c);
+	int mat[r][c];
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			scanf("%d",&mat[i][j]);
+		}
+	}
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			
+			
+			if(i==0 || j==0 || i==r-1 || j==c-1){
+				sum=sum+mat[i][j];
+			}
+			
+		}
+	
+}
+	printf("%d",sum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
