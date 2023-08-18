8049db21d9d3359da6b9901ba8a0c23441f5eb6b
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2023-08-18 18:31:42 +0530

    #include<stdio.h> int main(){     int i,arr[100],n,c=0;     scanf("%d",&n);     for(i=0;i<n;i++){         scanf("%d",&arr[i]);     }     for(i=0;i<n;i+=2){         if(arr[i]%2!=0){             printf("False");             c++;             break;         }     }     if(c==0){         printf("True");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int i,arr[100],n,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i+=2){
+        if(arr[i]%2!=0){
+            printf("False");
+            c++;
+            break;
+        }
+    }
+    if(c==0){
+        printf("True");
+    }
+}
